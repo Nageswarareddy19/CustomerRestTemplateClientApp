81c096612f54a4d567f6d90b3aa7207befd685d7
Nageswarareddy19 <nageswarareddykandikonda@gmail.com>
2019-11-28 16:51:31 +0530

    logging is enabled

--- src/main/java/com/customer/client/Proj07CustomerClienttExampleApplication.java
+++ src/main/java/com/customer/client/Proj07CustomerClienttExampleApplication.java
@@ -2,30 +2,42 @@ package com.customer.client;
 
 import java.util.Scanner;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.http.ResponseEntity;
 
 import com.customer.client.model.Customer;
 import com.customer.client.service.CustomerClientService;
 
 @SpringBootApplication
 public class Proj07CustomerClienttExampleApplication {
+	static Logger logger=LoggerFactory.getLogger(Proj07CustomerClienttExampleApplication.class);
+	
+	public Proj07CustomerClienttExampleApplication() {
+		logger.debug("CustomerClienttExampleApplication()::Instantiated");
+		logger.info("CustomerClienttExampleApplication()::Instantiated");
+		
+	}
 
 	public static void main(String[] args) {
+		
+		logger.debug("main() method is called");
+		
 		CustomerClientService service = null;
 		Scanner sc = null;
 		String customerEmail = null;
 		ConfigurableApplicationContext ctx = SpringApplication.run(Proj07CustomerClienttExampleApplication.class, args);
 		service = ctx.getBean(CustomerClientService.class);
-		
-		  Customer customer = service.getCustomerByEmail("naveen@gmail.com");
-		  System.out.println(customer); Customer customerById =
-		  service.getCustomerById(23); System.out.println(customerById); String
-		  allCustomerEmails = service.getAllCustomerEmails();
-		  System.out.println(allCustomerEmails);
-		 
+
+		Customer customer = service.getCustomerByEmail("naveen@gmail.com");
+		System.out.println(customer);
+		Customer customerById = service.getCustomerById(23);
+		System.out.println(customerById);
+		String allCustomerEmails = service.getAllCustomerEmails();
+		System.out.println(allCustomerEmails);
+
 		/*
 		 * Customer customer2 = new Customer(); sc = new Scanner(System.in);
 		 * System.out.println("Enter customer email"); customerEmail = sc.next();
@@ -35,7 +47,8 @@ public class Proj07CustomerClienttExampleApplication {
 		 * service.addCustomerDetails(customer2); System.out.println(details.getBody() +
 		 * "==" + details.getStatusCodeValue());
 		 */
-		sc.close();
+		logger.debug("main() method is ended");
+		logger.info("main() method is sucessfully executed");
 
 	}
 
--- src/main/java/com/customer/client/service/CustomerClientService.java
+++ src/main/java/com/customer/client/service/CustomerClientService.java
@@ -1,5 +1,7 @@
 package com.customer.client.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpEntity;
@@ -13,70 +15,101 @@ import com.customer.client.model.Customer;
 
 @Service
 public class CustomerClientService {
+
+	Logger logger = LoggerFactory.getLogger(CustomerClientService.class);
+
+	public CustomerClientService() {
+		logger.debug("CustomerClientService::Instantiated");
+		logger.info("CustomerClientService::Instantiated");
+	}
+
 	@Autowired
 	private Environment env;
 
-
 	@Autowired
 	private RestTemplate restTemplate;
-	
-	
-
 
 	public ResponseEntity<String> addCustomerDetails(Customer customer) {
+		logger.debug("addCustomerDetails() method  is started");
 		HttpHeaders header = new HttpHeaders();
 		header.add("content-type", "application/json");
 		// header.setContentType(MediaType.APPLICATION_JSON);
 		HttpEntity<Customer> httpEntity = new HttpEntity<>(customer, header);
 
-		ResponseEntity<String> entity = restTemplate.postForEntity(env.getProperty("ADD_CUSTOMER_DETAILS"), httpEntity, String.class);
+		ResponseEntity<String> entity = restTemplate.postForEntity(env.getProperty("ADD_CUSTOMER_DETAILS"), httpEntity,
+				String.class);
 		int status = entity.getStatusCode().value();
 
 		if (status == 201) {
-				String body = entity.getBody();
-			return new ResponseEntity<String>(body,HttpStatus.CREATED);
+			String body = entity.getBody();
+			logger.debug("addCustomerDetails() method  is ended");
+			logger.info("addCustomerDetails() method  is sucessfully completed");
+
+			return new ResponseEntity<String>(body, HttpStatus.CREATED);
 		} else {
+			logger.debug("null value is returned");
+			logger.info("response is not getting from CustomerRestApi");
 			return null;
 		}
 
 	}
 
 	public Customer getCustomerByEmail(String email) {
+		logger.debug("getCustomerByEmail() method is called");
 
-		ResponseEntity<Customer> entity = restTemplate.getForEntity(env.getProperty("GET_CUSTOMER_BY_EMAIL"), Customer.class, email);
+		ResponseEntity<Customer> entity = restTemplate.getForEntity(env.getProperty("GET_CUSTOMER_BY_EMAIL"),
+				Customer.class, email);
 		int status = entity.getStatusCode().value();
 		if (status == 200) {
 			Customer body = entity.getBody();
+
+			logger.debug("getCustomerByEmail() method is ended");
+			logger.info("getCustomerByEmail() method is sucessfully executed");
 			return body;
+
 		} else {
+			logger.debug("null value is returned");
+			logger.info("response is not getting from CustomerRestApi");
 			return null;
 		}
 
 	}
 
 	public Customer getCustomerById(Integer customerID) {
-		ResponseEntity<Customer> entity = restTemplate.getForEntity(env.getProperty("GET_CUSTOMER_BY_ID") + customerID, Customer.class);
+		logger.debug("getCustomerByID() method is called");
+		ResponseEntity<Customer> entity = restTemplate.getForEntity(env.getProperty("GET_CUSTOMER_BY_ID") + customerID,
+				Customer.class);
 
 		int status = entity.getStatusCode().value();
 
 		if (status == 200) {
 			Customer body = entity.getBody();
+			logger.debug("getCustomerByID() method is ended");
+			logger.info("getCustomerByID() method is sucessfully completed");
 			return body;
 
 		} else {
+			logger.debug("null value is returned");
+			logger.info("response is not getting from CustomerRestApi");
 			return null;
 		}
 
 	}
 
 	public String getAllCustomerEmails() {
+		logger.debug("getAllCustomerEmails() method is called");
 
-		ResponseEntity<String> entity = restTemplate.getForEntity(env.getProperty("GET_ALL_CUSTOMER_EMAILS"), String.class);
+		ResponseEntity<String> entity = restTemplate.getForEntity(env.getProperty("GET_ALL_CUSTOMER_EMAILS"),
+				String.class);
 		int status = entity.getStatusCode().value();
 		if (status == 200) {
 			String body = entity.getBody();
+			logger.debug("getAllCustomerEmails() method is ended");
+			logger.info("getAllCustomerEmails() method is sucessfully executed");
 			return body;
 		} else {
+			logger.debug("null value is returned");
+			logger.info("response is not getting from CustomerRestApi");
 			return null;
 		}
 
